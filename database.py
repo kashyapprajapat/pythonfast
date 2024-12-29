9d0a06b80e8c3b912756ff08214e00c5b3f80fe5
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-12-29 11:27:06 +0530

    update datbase.py

--- database.py
+++ database.py
@@ -5,12 +5,11 @@ from motor.motor_asyncio import AsyncIOMotorClient
 # Load environment variables from .env file
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI ="mongodb+srv://kashyap:kashyap14kp@cluster0.jp9de.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "test_db"  # Replace with your database name
 COLLECTION_NAME = "test_collection"  # Replace with your collection name
 
-# Print MongoDB URI
-print("MongoDB URI:", MONGO_URI)
+
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DATABASE_NAME]
