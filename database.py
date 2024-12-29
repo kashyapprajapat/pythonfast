133bd63b2b06fe7efa2615795de83da9fba7e9b8
kashyapprajapat <prajapatikashyap14@gmail.com>
2024-12-29 11:23:54 +0530

    update datbase.py

--- database.py
+++ database.py
@@ -9,6 +9,9 @@ MONGO_URI = os.getenv("MONGO_URI")
 DATABASE_NAME = "test_db"  # Replace with your database name
 COLLECTION_NAME = "test_collection"  # Replace with your collection name
 
+# Print MongoDB URI
+print("MongoDB URI:", MONGO_URI)
+
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
